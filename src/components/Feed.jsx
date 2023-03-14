aff215bd3d937243f9c0a8c0d8a5797c15ff94b9
alekzandaryi <alekzandaryi@students.npmg.org>
2023-03-14 12:12:02 +0200

    Fetching form yt rapid api P.1

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { Sidebar, Videos } from "./";
+import {fetchFromAPI} from '../utils/fetchFromAPI'
 
 const Feed = () => {
+
+	const [selectedCategory, setSelectedCategory] = useState(`New`)
+	useEffect(() => {
+		fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+	}, [selectedCategory])
+
 	return (
 		<Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
 			<Box
@@ -12,7 +19,10 @@ const Feed = () => {
 					px: { sx: 0, md: 2 },
 				}}
 			>
-				<Sidebar />
+				<Sidebar 
+				selectedCategory = {selectedCategory}
+				setSelectedCategory = {setSelectedCategory}
+				/>
 
 				<Typography
 					className="copyright"
@@ -35,7 +45,7 @@ const Feed = () => {
 						color: "white",
 					}}
 				>
-					New <span style={{ color: "#fc1503" }}>Videos</span>
+					{selectedCategory} <span style={{ color: "#fc1503" }}>Videos</span>
 				</Typography>
 
 				<Videos videos={[]} />
